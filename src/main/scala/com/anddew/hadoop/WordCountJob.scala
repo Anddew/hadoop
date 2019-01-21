78060988773302aaea8873ca90de525869739b87
Andrei Bylinovich <abylinovich@qulix.com>
2019-01-21 08:55:34 +0300

    Add input/output paths.

--- src/main/scala/com/anddew/hadoop/WordCountJob.scala
+++ src/main/scala/com/anddew/hadoop/WordCountJob.scala
@@ -11,17 +11,19 @@ import org.apache.hadoop.util.{Tool, ToolRunner}
 class WordCountJob extends Configured with Tool {
 
   override def run(strings: Array[String]): Int = {
+    require(strings.length >= 2, "Type input and output paths.")
+
     val job: Job = Job.getInstance(getConf, "WordCount")
     job.setJarByClass(getClass)
 
-    FileInputFormat.addInputPath(job, new Path("input"))
+    FileInputFormat.addInputPath(job, new Path(strings(1)))
     job.setInputFormatClass(classOf[TextInputFormat])
 
     job.setMapperClass(classOf[WordCountMapper])
     job.setReducerClass(classOf[WordCountReducer])
     job.setCombinerClass(classOf[WordCountReducer])
 
-    FileOutputFormat.setOutputPath(job, new Path("output"))
+    FileOutputFormat.setOutputPath(job, new Path(strings(2)))
     job.setOutputFormatClass(classOf[TextOutputFormat[_,_]])
     job.setOutputKeyClass(classOf[Text])
     job.setOutputValueClass(classOf[IntWritable])
